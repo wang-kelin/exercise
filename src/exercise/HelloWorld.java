bd62cd90ced16816e1cd7771f49499d8148e0621
kelin.wang <kelin.wang@qq.com>
2018-12-23 17:54:14 +0800

    修改

--- src/exercise/HelloWorld.java
+++ src/exercise/HelloWorld.java
@@ -2,7 +2,14 @@ package exercise;
 
 public class HelloWorld {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
+		first();
+	}
+
+	public static void first() {
+
+		String args = "This is my first github repository";
+		System.out.println(args);
 	}
 }
