e5e4a498a1ed7a3f1fac97f6600d577c3f63dc73
kelin.wang <kelin.wang@qq.com>
2018-12-23 20:20:53 +0800

    追加

---
+++ src/exercise/Apple.java
@@ -0,0 +1,34 @@
+package exercise;
+
+public class Apple {
+
+	private int id;
+	
+	private String type;
+	
+	private String sale;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSale() {
+		return sale;
+	}
+
+	public void setSale(String sale) {
+		this.sale = sale;
+	}
+}
--- src/exercise/HelloWorld.java
+++ src/exercise/HelloWorld.java
@@ -5,11 +5,23 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		first();
+		Apple apple = second();
+		System.out.println("编号:" + apple.getId() + "-类型:" + apple.getType() + "-售价:" + apple.getSale());
 	}
 
 	public static void first() {
 
 		String args = "This is my first github repository";
 		System.out.println(args);
+	}
+	
+	public static Apple second(){
+		
+		Apple apple = new Apple();
+		apple.setId(1);
+		apple.setType("ss");
+		apple.setSale("50");
+		
+		return apple;
 	}
 }
